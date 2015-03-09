89a9b34542d1ede3071125a815e96f907bcde40d
Nick Cuthbert <nick@cuthbert.co.za>
2015-03-09 16:43:45 +0200

    Rocket launch

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -8,7 +8,9 @@ public class Checkpoint : MonoBehaviour
     public Transform StartPoint;
     public Transform EndPoint;
     private Material _material;
-    private const int VertexCount=20;
+    private const int VertexCount=50;
+	public const int NumberOfPeriods = 10;
+	public const float Amplitude = 0.5f;
     // Use this for initialization
     private LineRenderer _renderer;
     
@@ -16,8 +18,9 @@ public class Checkpoint : MonoBehaviour
     {
         _renderer = gameObject.AddComponent<LineRenderer>();        
         _renderer.SetVertexCount(VertexCount);
-        _renderer.SetWidth(0.2f,0.1f);
+        _renderer.SetWidth(0.05f,0.05f);
         _renderer.sortingLayerName = "Player";
+		_renderer.sortingOrder =0;
         _renderer.SetColors(new Color(0.25f, 1, 0.1f), new Color(1, 0,0));
         _material= new Material(Shader.Find("Particles/Additive"));
         _renderer.material =_material;
@@ -25,29 +28,28 @@ public class Checkpoint : MonoBehaviour
     }
 
     private float _progress = 0;
-    private const float Amplitude=0.2f;
+    
     // Update is called once per frame
     private void Update()
     {
         _renderer = GetComponent<LineRenderer>();
-        float deltaY = (EndPoint.position - StartPoint.position).y;
-        float deltaX = (EndPoint.position - StartPoint.position).x;
-        if (Mathf.Abs(deltaX)>0)
-        {
-            for (int i = 0; i < VertexCount; i++)
-            {
-                _renderer.SetPosition(i, new Vector3(i / (float)VertexCount * deltaX + StartPoint.position.x, StartPoint.position.y + Amplitude * Mathf.Sin(i * _progress * 20f),StartPoint.position.z));
-            }
-        }
-        else
-        {
-            for (int i = 0; i < 20; i++)
-            {
-                _renderer.SetPosition(i, new Vector3(StartPoint.position.x + Amplitude * Mathf.Sin(i * _progress * 20f), i / (float)VertexCount * deltaY + StartPoint.position.y, StartPoint.position.z));
-            }
-        }
-        _progress += Time.deltaTime;
+		var delta = (EndPoint.position - StartPoint.position);
+		var length = delta.magnitude;
+		var angle = delta.normalized;
+
+		var frequency = 1f/ NumberOfPeriods;
+
+		for (int i=0; i<VertexCount; ++i) {
+			float height=Amplitude*Mathf.Sin (frequency*i+_progress*Mathf.PI);
+			float position=(length/(float)VertexCount)*i;
+			_renderer.SetPosition(i, new Vector3(angle.x*height+StartPoint.position.x+position*angle.y,position*angle.x+angle.y*height+StartPoint.position.y,StartPoint.position.z));
+		}
+        
+						
+			
+		_progress += Time.deltaTime;
+	}
+        
                 
         
-    }
 }
