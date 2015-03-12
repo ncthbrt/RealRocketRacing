1ff930e1dfb5afd65c7ac589dc87502f77f84c1a
Nick Cuthbert <nick@cuthbert.co.za>
2015-03-12 04:29:00 +0200

    started building Industry level

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public Rigidbody2D Rocket;
+    public float MaxSpeed;
+    public float Mass;
+        
+    
+    public float DeadZone;    
+
+    private void Start()
+    {    
+    }
+
+
+
+	void FixedUpdate ()
+	{
+        Vector2 delta = Rocket.position - new Vector2(transform.position.x, transform.position.y);            
+        transform.Translate(delta);	    
+	}
+}
